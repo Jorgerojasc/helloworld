cad812b4f50b55ee4b79b0e2f20212e22222e811
Jorgerojasc <jorgerojas1c@hotmail.com>
2019-09-26 17:11:31 -0500

    JSON

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
-
+const arreglo = []
+const json = {}
 //Manejar las peticiones que haga a la direccion http://127.0.0.1:3000/
-app.get("/",(req, res)=>{
+app.get("/:nombre/:edad",(req, res)=>{
     //console.log(req)/
     res.send("Hola Mundo")
-  
+    //arreglo.push(req.params.id)/
+    json.nombre = req.params.nombre
+    json.edad = req.params.edad
+    console.log(json.nombre+":"+json.edad)
+    console.log(req.params.nombre+":"+req.params.edad)
     
 })
 
@@ -15,3 +20,4 @@ app.listen(3000,()=>{
     console.log("Server is running.......")
 })
 /*Para instalar las carpetas node_modules de un proyecto ponemos: npm install, sobre el la carpeta del proyecto*/
+
